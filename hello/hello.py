9a665c4762f68131c0366b7af4ec8f8f70c4b8f4
Rhianon Hoisington <lightishredgummi@gmail.com>
2023-10-30 22:57:20 +0000

    tests with hello, kattis-cli not working

--- hello/hello.py
+++ hello/hello.py
@@ -1,7 +1,6 @@
-import sys
-
 def main() -> None:
     print("Hello World!")
+
 
 if __name__ == "__main__":
     main()
--- script.sh
+++ script.sh
@@ -3,8 +3,8 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo "Working directory is $SCRIPT_DIR"
 
-chown user:users /home/user/.zsh_history
-chown user:users /home/user/.gitconfig
-# chown user:users --recursive /home/user
-PATH='${SCRIPT_DIR}:$PATH'
-export PATH
+#chown user:users /home/user/.zsh_history
+#chown user:users /home/user/.gitconfig
+chown user:users --recursive /home/user/. &> /dev/null
+echo "#!/bin/sh" > /home/user/kattis-cli/kattis
+echo 'python "${KATTIS_CLI}/submit.py" "$@"' >> /home/user/kattis-cli/kattis
